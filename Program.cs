020ec970aac231f802a7a8532ab5a0f5c4feba81
Etec Prof. Horácio Augusto da Silveira <dennissilvageneroso@gmail.com>
2026-02-10 22:25:53 -0300

    Aula 01 - Hello World

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Digite o seu nome");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"Seu nome tem {nome.Length} caracteres");
+
+            Console.WriteLine("Digite a data do seu nascimento");
+            
+            DateTime dtNascimento = DateTime.Parse(Console.ReadLine());
+
+            int qtdDiasVividos = DateTime.Now.Subtract(dtNascimento).Days;
+
+            Console.WriteLine("Dias Vividos: " + qtdDiasVividos);
+
+            Console.ReadKey();
+        }
+    }
+}
